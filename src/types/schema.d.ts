3f4debcc0655e666d63759e1b517bbc57300128e
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2023-12-27 15:31:21 +0800

    Update schema

--- src/types/schema.d.ts
+++ src/types/schema.d.ts
@@ -1,3 +1,12 @@
+interface TypedObject {
+  _type: string,
+  _key: string,
+}
+
+type GenericBlock = TypedObject & {
+  [key: string]: any,
+}
+
 interface AccessibleImageMetadata {
   blurHash: string,
   width: number,
@@ -11,13 +20,199 @@ interface AccessibleImageAsset {
 interface AccessibleImage {
   alt: string,
   asset: AccessibleImageAsset,
+  // RichText is essentially a block
+  caption?: GenericBlock[],
 }
 
-interface GenericBlock {
-  [key: string]: unknown,
+// Copied from Shiki, LICENSE: MIT
+// We can't import from shiki as this will break this ambient file
+// Note: Update the following if Shiki updates the language it supports
+type BuiltinLanguage = 'abap'
+  | 'actionscript-3'
+  | 'ada'
+  | 'apache'
+  | 'apex'
+  | 'apl'
+  | 'applescript'
+  | 'ara'
+  | 'asm'
+  | 'astro'
+  | 'awk'
+  | 'ballerina'
+  | 'bat' | 'batch'
+  | 'beancount'
+  | 'berry' | 'be'
+  | 'bibtex'
+  | 'bicep'
+  | 'blade'
+  | 'c'
+  | 'cadence' | 'cdc'
+  | 'clarity'
+  | 'clojure' | 'clj'
+  | 'cmake'
+  | 'cobol'
+  | 'codeql' | 'ql'
+  | 'coffee'
+  | 'cpp' | 'c++'
+  | 'crystal'
+  | 'csharp' | 'c#' | 'cs'
+  | 'css'
+  // | 'csv'
+  | 'cue'
+  | 'cypher' | 'cql'
+  | 'd'
+  | 'dart'
+  | 'dax'
+  | 'diff'
+  | 'docker' | 'dockerfile'
+  | 'dream-maker'
+  | 'elixir'
+  | 'elm'
+  | 'erb'
+  | 'erlang' | 'erl'
+  | 'fish'
+  | 'fsharp' | 'f#' | 'fs'
+  | 'gdresource'
+  | 'gdscript'
+  | 'gdshader'
+  | 'gherkin'
+  | 'git-commit'
+  | 'git-rebase'
+  | 'glimmer-js' | 'gjs'
+  | 'glimmer-ts' | 'gts'
+  | 'glsl'
+  | 'gnuplot'
+  | 'go'
+  | 'graphql' | 'gql'
+  | 'groovy'
+  | 'hack'
+  | 'haml'
+  | 'handlebars' | 'hbs'
+  | 'haskell' | 'hs'
+  | 'hcl'
+  | 'hjson'
+  | 'hlsl'
+  | 'html'
+  | 'http'
+  | 'imba'
+  | 'ini' | 'properties'
+  | 'java'
+  | 'javascript' | 'js'
+  | 'jinja-html'
+  | 'jison'
+  | 'json'
+  | 'json5'
+  | 'jsonc'
+  | 'jsonl'
+  | 'jsonnet'
+  | 'jssm' | 'fsl'
+  | 'jsx'
+  | 'julia'
+  | 'kotlin' | 'kt' | 'kts'
+  | 'kusto' | 'kql'
+  | 'latex'
+  | 'less'
+  | 'liquid'
+  | 'lisp'
+  | 'logo'
+  | 'lua'
+  | 'make' | 'makefile'
+  | 'markdown' | 'md'
+  | 'marko'
+  | 'matlab'
+  | 'mdc'
+  | 'mdx'
+  | 'mermaid'
+  | 'mojo'
+  | 'narrat' | 'nar'
+  | 'nextflow' | 'nf'
+  | 'nginx'
+  | 'nim'
+  | 'nix'
+  | 'objective-c' | 'objc'
+  | 'objective-cpp'
+  | 'ocaml'
+  | 'pascal'
+  | 'perl'
+  | 'php'
+  | 'plsql'
+  | 'postcss'
+  | 'powerquery'
+  | 'powershell' | 'ps' | 'ps1'
+  | 'prisma'
+  | 'prolog'
+  | 'proto'
+  | 'pug' | 'jade'
+  | 'puppet'
+  | 'purescript'
+  | 'python' | 'py'
+  | 'r'
+  | 'raku' | 'perl6'
+  | 'razor'
+  | 'reg'
+  | 'rel'
+  | 'riscv'
+  | 'rst'
+  | 'ruby' | 'rb'
+  | 'rust' | 'rs'
+  | 'sas'
+  | 'sass'
+  | 'scala'
+  | 'scheme'
+  | 'scss'
+  | 'shaderlab' | 'shader'
+  | 'shellscript' | 'bash' | 'sh' | 'shell' | 'zsh'
+  | 'shellsession' | 'console'
+  | 'smalltalk'
+  | 'solidity'
+  | 'sparql'
+  | 'splunk' | 'spl'
+  | 'sql'
+  | 'ssh-config'
+  | 'stata'
+  | 'stylus' | 'styl'
+  | 'svelte'
+  | 'swift'
+  | 'system-verilog'
+  | 'tasl'
+  | 'tcl'
+  | 'tex'
+  | 'toml'
+  | 'tsx'
+  | 'turtle'
+  | 'twig'
+  | 'typescript' | 'ts'
+  | 'v'
+  | 'vb' | 'cmd'
+  | 'verilog'
+  | 'vhdl'
+  | 'viml' | 'vim' | 'vimscript'
+  | 'vue-html'
+  | 'vue'
+  | 'vyper' | 'vy'
+  | 'wasm'
+  | 'wenyan' | '文言'
+  | 'wgsl'
+  | 'wolfram'
+  | 'xml'
+  | 'xsl'
+  | 'yaml' | 'yml'
+  | 'zenscript'
+  | 'zig';
+
+interface CodeBlock {
+  language: BuiltinLanguage,
+  code: string,
 }
 
-type PostBlockContent = AccessibleImage | GenericBlock;
+interface Callout {
+  style: 'info' | 'warning' | 'danger',
+  title: string,
+  // RichText is essentially a block
+  content: GenericBlock[],
+}
+
+type PostBlockContent = TypedObject & (AccessibleImage | Callout) | GenericBlock;
 
 interface Post {
   title: string,
