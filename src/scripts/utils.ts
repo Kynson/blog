b4b2b2156927f4f8f66141503edf212e76c055eb
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2024-01-06 15:26:23 +0800

    Patch UI bugs
    - Fix bold text font-weight inconsistant
    - Fix PortableTextTerminalRecording hight not updated correctly
    - Fix missing time zone

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -2,8 +2,9 @@ export function formatDateString(dateString: string) {
   return new Date(dateString)
     .toLocaleString('en-HK', {
       dateStyle: 'medium',
-      timeStyle: 'medium',
+      timeStyle: 'long',
       hour12: false,
+      timeZone: 'Asia/Hong_Kong',
     });
 }
 
@@ -11,4 +12,12 @@ export function delay(milliseconds: number): Promise<void> {
   return new Promise((resolve, _) => {
     setTimeout(resolve, milliseconds);
   });
+}
+
+export function debounce(callback: (...arg: any[]) => any, milliseconds: number) {
+  let previousTimeout: number | string | NodeJS.Timeout;
+  return (...arg: any[]) => {
+    clearTimeout(previousTimeout);
+    previousTimeout = setTimeout(() => callback(arg), milliseconds);
+  }
 }
