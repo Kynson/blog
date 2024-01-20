1bb1accfc9245be51a68fcd8619826d7767982d2
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2024-01-20 09:33:58 +0800

    [Skip CI] Patch Code.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,19 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
 import { sanityIntegration as sanity } from '@sanity/astro';
-// import viteWasmPlugin from 'vite-plugin-wasm';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
-    wasmModuleImports: true,
+    wasmModuleImports: true
   }),
   image: {
     service: passthroughImageService(),
   },
-  integrations: [
-    sanity({
-      projectId: '471rt6us',
-      dataset: 'production',
-      useCdn: false
-    })
-  ],
-  // vite: {
-  //   plugins: [viteWasmPlugin()],
-  // }
+  integrations: sanity({
+    projectId: '471rt6us',
+    dataset: 'production',
+    useCdn: false
+  }),
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,9 @@
 /// <reference types="astro/client" />
 /// <reference types="@sanity/astro/module" />
+
+// Defined by cloudflare adapter
+// https://github.com/withastro/adapters/blob/09306d51bb83f584ab35509881c83618a2752445/packages/cloudflare/src/utils/wasm-module-loader.ts#L12
+declare module '*.wasm?module' {
+  const wasmModule: WebAssembly.Module;
+  export default wasmModule;
+}
