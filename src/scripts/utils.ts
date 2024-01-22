80730419ae1fb78988b8a9a9355266c13168efcf
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2024-01-22 14:54:46 +0800

    Implement search and related functions & pages

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -18,6 +18,29 @@ export function debounce(callback: (...arg: any[]) => any, milliseconds: number)
   let previousTimeout: number | string | NodeJS.Timeout;
   return (...arg: any[]) => {
     clearTimeout(previousTimeout);
-    previousTimeout = setTimeout(() => callback(arg), milliseconds);
+    previousTimeout = setTimeout(() => callback(...arg), milliseconds);
   }
+}
+
+export class AbortErrorWithSignal extends DOMException {
+  signal: AbortSignal;
+
+  constructor(signal: AbortSignal, message?: string, name?: string) {
+    super(message, name);
+    this.signal = signal;
+  }
+}
+
+export function abortableFetch(input: RequestInfo | URL, init?: RequestInit, abortController?: AbortController) {
+  const { signal } = abortController;
+
+  return fetch(input, { ...(init || {}), signal }).catch((error: TypeError | DOMException) => {
+    const { message, name } = error;
+
+    if (!(error instanceof DOMException) || name !== 'AbortError') {
+      throw error;
+    }
+
+    throw new AbortErrorWithSignal(signal, message, name);
+  });
 }
